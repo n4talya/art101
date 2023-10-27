1c91d23a65c3af2f01f99a867a94e83200fe0dc7
n4talya <146698750+n4talya@users.noreply.github.com>
2023-10-26 17:56:04 -0700

    lab 5 changes

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Hangman An implementation of the game 
+// Author: Natalya Gonzalez <naangonz@ucsc.edu>
+// Date: October 2023
 
 // Constants
 
@@ -19,3 +19,17 @@ function main() {
 
 // let's get this party started
 main();
+
+//Two or more player game
+//Player 1 comes up with a word or phrase
+  //Before the game starts there is a gallow that is made up of 4 lines
+  //Player 1 draws a line for every letter in the phrase
+//The other player(s) try to guess the word/phrase
+  //For every correct letter, Player 1 fills in the letter on top of the line they drew throughout the phrase, even if included multiple times
+  //For every incorrect letter, Player 1 writes the letter in the empty top section above the gallow, Player 1 also draws a body part of a stick figure starting with the head
+//The other player(s) take turns guessing the letters
+//If one of the players is able to guess the word/phrase correctly then they win
+  //If the player guesses incorrectly then you draw another body part onto the stick figure
+  //Players are able to guess wrong 7 times before they lose
+//Once the hangman is completely drawn Player 1 wins
+//If the other player(s) are able to solve the word/phrase correctly then they win

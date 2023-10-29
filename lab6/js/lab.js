3b3c7f1f07cd1b21621fcca9bedfd081519ac294
n4talya <146698750+n4talya@users.noreply.github.com>
2023-10-29 16:08:17 -0700

    lab 6 changes

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,31 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Experimenting with arrays and objects in javascript
+// Author: Natalya Gonzalez
+// Date: October 27th 2023
+
+//Define Variables
+var myTransport = ["2008 Kia Amanti", "bike", "walking", "bus"];
+
+//create an object for my main ride
+var myMainRide = {
+  make: "Kia",
+  model: "Amanti",
+  color: "Silver",
+  year: 2008,
+  //we can define a function within our object (called a method)
+  //that uses the value of year above (using this.year)
+  age: function () {
+    return 2023 - this.year;
+  }
+
+}
+
+//output
+document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
+//this little trick allows us to write an object to the document 
+document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
+
 
 // Constants
 
